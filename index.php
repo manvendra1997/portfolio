b993581517bb00a084fb1a89491f3373230cd155
manvendra1997 <59593118+manvendra1997@users.noreply.github.com>
2020-01-08 15:42:31 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,412 @@
+<html>
+
+<head>
+    <title>portfolio</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="row no-gutters">
+                <div class="col-lg-4 col-md-4">
+                    <img src="img/manvendra.jpg">
+                </div>
+                <div class="col-lg-8 col-md-8">
+                    <div class="d-flex flex-column">
+                        <div class="p-5 bg-dark text-white">
+                            <div class="d-flex flex-row justify-content-between align-items-center">
+                                <h1 class="display-4">Manvendra Singh</h1>
+                                 
+
+                                 <a href="https://www.facebook.com/manvendra.S.Azad/" style="color: rgb(255,255,255)" target="_blank">
+                                <div><i class="fa fa-facebook"></i></div>
+                               </a>
+                               
+                                 <a href="https://mail.google.com/mail/u/0/#inbox/" style="color: rgb(255,255,255)" target="_blank">
+                                <div><i class="fa fa-google-plus"></i></div>
+                            </a>
+                               
+                               <a href="https://www.instagram.com/manvendra_s_azad/" style="color: rgb(255,255,255)" target="_blank">
+                               <div><i class="fa fa-instagram"></i> </div>
+                               </a>
+
+                                 <a href="https://www.linkedin.com/in/manvendra-singh-gautam-168597195/" style="color: rgb(255,255,255)" target="_blank">
+                                <div><i class="fa fa-linkedin-square"></i></div>
+                            </a>
+
+
+                            </div>
+
+                        </div>
+                        <div class="p-3 text-white bg-black">
+                            Web Devlopment
+                        </div>
+                        <div class="d-flex flex-row text-center text-white">
+                            <div class="p-4 bg-success font-div" data-toggle="collapse"data-target="#home">
+                                 <div><i class="fa fa-home fa-2x d-block"></i>Home</div>
+
+                            </div>
+                             <div class="p-4 bg-danger font-div" data-toggle="collapse"data-target="#resume">
+                                 <div><i class="fa fa-graduation-cap fa-2x d-block"></i>Resume</div>
+
+                            </div>
+                             <div class="p-4 bg-primary font-div" data-toggle="collapse"data-target="#work">
+                                 <div><i class="fa fa-folder fa-2x d-block"></i>Work</div>
+
+                            </div>
+                             <div class="p-4 bg-warning font-div" data-toggle="collapse"data-target="#Contact">
+                                 <div><i class="fa fa-envelope fa-2x d-block"></i>Contact</div>
+
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+
+<!-- home -->
+
+
+
+<div class="container">
+    <div class="collapse"id="Home">
+        <div class="p-5 text-white bg-success">
+            <h2>My Resume</h2>
+            <p>hsdkflhsajkfhskjfhskhfksjhfkshfkshfkshdfkshfklshfkshdfkshfksj</p>
+        </div>
+        <div class="card card-body">
+            <h2>My Skills</h2>
+            <p>hsdkflhsajkfhskjfhskhfksjhfkshfkshfkshdfkshfklshfkshdfkshfksj</p>
+            <h4>HTML</h4>
+            <div class="progress mb-3">
+                <div class="progress-bar progress-bar-striped progress-bar-animated " style="width: 100%;"></div>
+            </div>
+            <h4>CSS</h4>
+            <div class="progress mb-3">
+                <div class="progress-bar progress-bar-striped progress-bar-animated " style="width: 100%;"></div>
+            </div>
+            <h4>Bootstrap</h4>
+            <div class="progress mb-3">
+                <div class="progress-bar progress-bar-striped progress-bar-animated " style="width: 80%;"></div>
+            </div>
+            <h4>PHP</h4>
+            <div class="progress mb-3">
+                <div class="progress-bar progress-bar-striped progress-bar-animated " style="width: 65%;"></div>
+            </div>
+            <h4>JAVA</h4>
+            <div class="progress mb-3">
+                <div class="progress-bar progress-bar-striped progress-bar-animated " style="width: 80%;"></div>
+            </div>
+            <h4>C</h4>
+            <div class="progress mb-3">
+                <div class="progress-bar progress-bar-striped progress-bar-animated " style="width: 70%;"></div>
+            </div>
+        </div>
+    </div>
+
+
+   <!--  resume -->
+
+
+    <div class="collapse" id="Resume">
+         <div class="p-5 text-white bg-danger">
+            <h2>My Resume</h2>
+            <p>I have one year experience.</p>
+        </div class="card card-body">
+             <h2>Where have I working</h2>
+              <p>hsdkflhsajkfhskjfhskhfksjhfkshfkshfkshdfkshfklshfkshdfkshfksj</p>
+              <!-- Card Image -->
+
+
+              <div class="card-deck text-center">
+                  <div class="card">
+                      <div class="card-header text-uppercase font-weight-bold">
+                          Web Development
+                      </div>
+                      <div class="card-body">
+                        <p class="mb-5">hsdkflhsajkfhskjfhskhfksjhfkshfkshfkshdfkshfklshfkshdfkshfksj</p>
+                          <p class="bg-dark text-white p-2">As a front_End Devloper</p>
+                            <p class="bg-dark text-white p-2">As a front_End Devloper</p>
+                      </div>
+                      <div class="card-footer">
+                          Date:2018-2019
+                      </div>
+                  </div>
+
+
+                   <div class="card">
+                      <div class="card-header text-uppercase font-weight-bold">
+                          Web Development
+                      </div>
+                      <div class="card-body">
+                        <p class="mb-5">hsdkflhsajkfhskjfhskhfksjhfkshfkshfkshdfkshfklshfkshdfkshfksj</p>
+                          <p class="bg-dark text-white p-2">As a front_End Devloper</p>
+                            <p class="bg-dark text-white p-2">As a front_End Devloper</p>
+                      </div>
+                      <div class="card-footer">
+                          Date:2018-2019
+                      </div>
+                  </div>
+
+
+
+                   <div class="card">
+                      <div class="card-header text-uppercase font-weight-bold">
+                          Web Development
+                      </div>
+                      <div class="card-body">
+                        <p class="mb-5">hsdkflhsajkfhskjfhskhfksjhfkshfkshfkshdfkshfklshfkshdfkshfksj</p>
+                          <p class="bg-dark text-white p-2">As a front_End Devloper</p>
+                            <p class="bg-dark text-white p-2">As a front_End Devloper</p>
+                      </div>
+                      <div class="card-footer">
+                          Date:2018-2019
+                      </div>
+                  </div>
+              </div>
+
+              <!-- resume manvendra -->
+
+             <div class="card-deck ">
+        <div class="card">
+
+          <div class="card-body">
+            <h2 class="text-left">Manvendra Singh</h2>
+            <h6>
+              <p class="ex1">Barola Noida Sector-49</p>
+              <p class="ex1">Uttar Pradesh, 201301</p>
+              <p class="ex1">Mob- 9161976991, 9452718518</p>
+              <p class="ex1">Email- manvendramca1@gmail.com</p>
+            </h6>
+          </div>
+          <div class="bg-dark text-white p-1">CAREER OBJECTIVES:</div>
+          <h6 class="card-body">
+            <p class="ex1">Interested in working with a reputed and well managed organization where my skill is utilized
+              to the fullest, there by leading to the organization as well as my career in the organization.</p>
+          </h6>
+          <div class="bg-dark text-white p-1">EDUCATIONAL QUALIFICATIONS: </div>
+          <h6 class="card-body">
+            <p class="ex1"> MCA with 70% from AKTU Lucknow University in 2020.</p>
+            <p class="ex1">BCA with 69.15 % from MCU Bhopal in 2018.</p>
+            <p class="ex1">12th with 83 % from UP Board in 2014.</p>
+            <p class="ex1">10th with 75 % from UP Board in 2012.</p>
+          </h6>
+          <div class="bg-dark text-white p-1">TECHNICAL SKILLS:</div>
+          <h6 class="card-body">
+
+            <p class="ex1"> <b>Languages</b> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp : &nbsp &nbsp &nbsp Node
+              js, Java (core), PHP (core), JavaScript, HTML5, CSS3, Bootstrap</p>
+            <p class="ex1"> <b>Database</b> &nbsp &nbsp &nbsp&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp: &nbsp &nbsp
+              &nbsp MySQL </p>
+            <p class="ex1"> <b>Operating Systems</b> &nbsp : &nbsp &nbsp &nbsp Windows XP/7/8/10 </p>
+            
+          </h6>
+          <!-- 
+          <div class="bg-dark text-white p-1">PROJECTS WORK SAMPLE::</div>
+          <h6 class="card-body">
+            <p class="ex1"> <a href="https://coox.in/" target="_blank">https://coox.in/ </a>(Backend in node js)</p>
+            <p class="ex1"> <a href="http://socialprofile.netlify.com/"
+                target="_blank">http://socialprofile.netlify.com/</a></p>
+            <p class="ex1"> <a href="http://sudhanshu647.000webhostapp.com/"
+                target="_blank">http://sudhanshu647.000webhostapp.com/</a></p>
+            <p class="ex1"> <a href="https://loginpagehtml.netlify.com/" target="_blank">http://loginpagehtml.com/</a>
+            </p>
+            <p class="ex1"> <a href="https://sscalc.netlify.com/" target="_blank">https://sscalc.netlify.com/</a></p>
+          </h6>
+
+          <div class="bg-dark text-white p-1">TECHNICAL SKILLS:</div>
+          <h6 class="card-body">
+            <p class="ex1"> <b>Company Name</b> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp : &nbsp &nbsp COOX</p>
+            <p class="ex1"> <b>Project title</b> &nbsp &nbsp &nbsp &nbsp&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp : &nbsp
+              &nbsp Online cook/chef booking </p>
+            <p class="ex1"> <b>Website</b> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp :
+              &nbsp &nbsp &nbsp<a href="https://coox.in/" target="_blank">https://coox.in/ </a></p>
+            <p class="ex1"> <b>Role</b> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp&nbsp &nbsp
+              &nbsp &nbsp &nbsp: &nbsp &nbsp&nbsp <B>Back-end developer</B> </p>
+            <p class="ex1"> <b>Duration</b> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp:
+              &nbsp &nbsp 8th September 19 to presents</p>
+          </h6>
+        -->
+          <div class="bg-dark text-white p-1">PERSONAL PROFILE: </div>
+
+          <h6 class="card-body">
+            <p class="ex1"><b>Father’s Name</b>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp :&nbsp &nbsp
+              &nbsp MR. Lakhan Singh</p>
+            <p class="ex1"><b>Date of Birth</b>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+              :&nbsp &nbsp &nbsp05/07/1997</p>
+            <p class="ex1">
+              <b>Sex</b>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+              :&nbsp &nbsp &nbspMale</p>
+            <p class="ex1"><b>Marital Status</b>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp :&nbsp
+              &nbsp &nbspUnmarried</p>
+            <p class="ex1"><b>Nationality</b>
+              &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp :&nbsp &nbsp &nbspIndian
+            </p>
+            <p class="ex1"><b>Language known</b>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp :&nbsp &nbsp &nbspHindi & English
+            </p>
+            <p class="ex1"><b>Permanent Address</b>&nbsp &nbsp :&nbsp &nbsp &nbspVill and Post Chhiriya Salempur, Dist Jalaun-285123 </p>
+          </h6>
+
+          </b>
+
+
+
+        </div>
+      </div>
+    
+       
+    </div>
+
+
+    <!-- work -->
+
+
+    <div class="collapse" id="work" >
+         <div class="p-5 text-white bg-primary">
+            <h2>My Work</h2>
+            <p>Here is my complete and uncomplete projects screenshot.</p>
+        </div class="card card-body">
+        <div class="card card-body">
+            <h3>This is MY portfolio Project</h3>
+        <p><img src="img/port.png" alt="Pineapple" style="width:60%;margin-right:15px;"><strong>4- &nbspThis
+            project is made in HTML , CSS ,Bootstrap , MySql.</strong>
+          <br>In this project we show my skill, resume, my work profile, contact form
+          <br><strong>Modules</strong>
+          <br><strong>*</strong> MY skill
+          <br><strong>*</strong> Resume
+          <br><strong>*</strong> My portfolio
+          <br><strong>*</strong> My work
+          <br><strong>*</strong> Contact me
+          <br>
+          <a href="https://sudhanshu647.000webhostapp.com" target="_blank">Click here to see full project</a>
+        </p>
+
+        <hr style="height:5px;border-top:5px solid #f00" />
+            
+        </div>
+    </div>
+
+
+
+    <!-- Contact -->
+     <?php
+      $result=" ";
+      if(isset($_POST['submit'])){
+        require 'phpmailer/PHPMailerAutoload.php';
+        $mail= new PHPMailer;
+        $mail ->Host='smtp-gmail.com';
+        $mail->Port=587;
+        $mail->SMTAuth=true;
+        $email->SMTPSecure='tls';
+        $mail->Username='manvendramonu445@gmail.com';
+        $mail->Password='1234';
+
+
+        $mail->setForm($_POST['email'],$_POST['name']);
+        $mail->addAddress('manvendras803@gmail.com');
+        $mail->addReplyTo($_POST['email'],$_POST['name']);
+
+        $mail->isHTML(true);
+        $email->subject='Form Submission: '.$_POST['subject'];
+        $mail->Body='<h1 align=center> Name: '.$_POST['name'].'<br>Email:'.$_POST['email'].'<br> Message: '.$_POST['msg'].'</h1>';
+
+        if(!$mail->send()){
+          $result="Somthing  went wrong.Please try again";
+
+        }
+        else{
+          $result="Thanks".$_POST['name']."for contacting us.w'll get back to you soon!";
+        }
+
+ 
+      }
+
+     ?>
+
+
+    <div class="collapse" id="Contact">
+         <div class="p-5 text-white bg-warning">
+            <h2>Contact Me</h2>
+            <p>hsdkflhsajkfhskjfhskhfksjhfkshfkshfkshdfkshfklshfkshdfkshfksj</p>
+        </div>
+        <div class="card card-body">
+            <div class="row justify-content-center" >
+                <div class="col-lg-8 col-md-8 col-12">
+                    
+                    <form>
+                      <div class="form-group">
+                        <div class="input-group input-group-lg">
+                             <span class="input-group-addon bg-warning text-white p-3">
+                            <i class="fa fa-user  "></i>
+                          </span>
+                            <input type="text" name="name" class="form-control bg-dark text-white" placeholder="Name">
+                        </div>         
+                   </div>
+                    <div class="form-group">
+                        <div class="input-group input-group-lg">
+                             <span class="input-group-addon bg-warning text-white p-3">
+                            <i class="fa fa-envelope  "></i>
+                          </span>
+                            <input type="email" name="email" class="form-control bg-dark text-white" placeholder="Email">
+                        </div>         
+                   </div>
+                    <div class="form-group">
+                        <div class="input-group input-group-lg">
+                             <span class="input-group-addon bg-warning text-white p-3">
+                            <i class="fa fa-envelope  "></i>
+                          </span>
+                            <input type="text" name="subject" class="form-control bg-dark text-white" placeholder="Massage">
+                        </div>         
+                   </div>
+                   <input type="submit" name="submit" id="submit" class="btn btn-danger" value="Send">
+
+
+                      </div>  
+
+
+                    </form>
+                </div>
+                
+            </div>
+            
+        </div>
+        
+    </div>
+</div>
+ 
+
+
+<div class="container">
+    <footer class="p-4 bg-dark text-center">
+      <a href="Manvendra.pdf" download>
+        <button class="p-2"> <i class="fa fa-download fa-2x "></i> Download</button>
+      </a>
+    </footer>
+</div>
+
+
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+
+        $('.font-div').click(function(){
+            $('.collapse').collapse('hide');
+        });
+    </script>
+</body>
+
+</html>
