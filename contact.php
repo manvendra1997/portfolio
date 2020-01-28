0d04deb1c5181982db4a969862c7ae319c174e04
manvendra1997 <59593118+manvendra1997@users.noreply.github.com>
2020-01-28 19:22:50 +0530

    Update and rename PHPMailerAutoload.php to contact.php

--- contact.php
+++ contact.php
@@ -1,11 +1,11 @@
-<?php
+o<?php
 if(!empty($_POST["submit"])) {
 	$name = $_POST["userName"];
 	$email = $_POST["userEmail"];
 	$phone =$_POST["phone"];
 	$subject = "Response From:" . $_POST["phone"] . ".\r\n";
 	$content = $_POST["content"];
-	$toEmail = "rajnishaptech.stm@gmail.com.com";
+	$toEmail = "manvendramonu445@gmail.com";
 	$mailHeaders = "From: " . $_POST["userName"] . "<". $email.">.\r\n";
 	if(mail($toEmail, $subject, $content, $mailHeaders)) {
 	$message = "<h3 style=color:green;>".$name.",Thank you !</h3>";
